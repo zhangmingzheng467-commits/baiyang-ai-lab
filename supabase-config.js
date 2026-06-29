affc906a2ea68eb365f325a43a900f4ed6b97950
zhangmingzheng467-commits <zhangmingzheng467@gmail.com>
2026-06-29 10:22:46 +0800

    Fix Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.BAIYANG_SUPABASE = {
-  url: "https://ajcsbwsxuzzynfdaylzo.supabase.co",
+  url: "https://ajcsbwsxuxzynfdaylzo.supabase.co",
   anonKey: "sb_publishable_Lw5neluAJ-zZjbASuGhr6g_JsebY_vO",
   bucket: "work-images"
 };
